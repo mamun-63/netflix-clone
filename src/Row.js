cd587a103eab0b9d7db5a59907e84ef0ecb45dcf
Mamun <mamun.cse63@gmail.com>
2020-09-19 01:36:31 +0600

    fetch from TMDB API, making rows, and style it

---
+++ src/Row.js
@@ -0,0 +1,56 @@
+import axios from './axios'
+import React, { useEffect, useState } from 'react'
+import './Row.css'
+
+const base_url = "http://image.tmdb.org/t/p/original/"
+
+function Row({ title, fetchUrl }) {
+
+  const [movies, setMovies] = useState([])
+
+  // A snippet of code which runs based on a specific condition
+  useEffect(() => {
+    // if [], run once the row/ components load and dont run again
+    // if [movies], here we add dependencied, means every time movies changes, it refires the code 
+    async function fetchData () {
+      const request = await axios.get(fetchUrl)
+      // https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213
+      
+      console.log(request) // to see the data structure we are getting back
+      // console.log(request.data.results) 
+
+      setMovies(request.data.results)
+      return request
+    }
+    fetchData()
+  }, [fetchUrl])
+
+  console.log(movies) 
+
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+
+      {/* constainer -> posters */}
+      <div className="row__posters">
+        {/* several row poster(s) */}
+        
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            className="row__poster" 
+            src={`${base_url}${movie.poster_path}`} 
+            alt={movie.name} 
+          />
+        ))}
+
+      </div>
+
+      
+    </div>
+  )
+  
+}
+
+export default Row 
