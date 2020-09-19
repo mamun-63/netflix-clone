4aad67077989339b464e4aeeb216063ecf8b71c6
Mamun <mamun.cse63@gmail.com>
2020-09-19 15:02:59 +0600

    add trailer popups

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,15 @@
 import axios from './axios'
 import React, { useEffect, useState } from 'react'
 import './Row.css'
+import Youtube from 'react-youtube'
+import movieTrailer from "movie-trailer"
 
 const base_url = "http://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl, isLargeRow }) {
 
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
 
   // A snippet of code which runs based on a specific condition
   useEffect(() => {
@@ -27,6 +30,33 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   console.log(movies) 
 
+  // youtube options
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    }
+  }
+
+  // whenever click on image, get the id and set it to trailerUrl
+  const handleClick = (movie) => {
+    if(trailerUrl){
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=g59rUQbVlIw
+          // we need this id, g59rUQbVlIw
+
+          const urlParams = new URLSearchParams(new URL(url).search) // ?v=g59rUQbVlIw
+          setTrailerUrl(urlParams.get('v'))  // g59rUQbVlIw
+        })
+        .catch((error) => console.log(error))
+    }
+  }
+
+
   return (
     <div className="row">
       {/* title */}
@@ -39,15 +69,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map(movie => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
             // src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-            alt={movie.name} 
+            alt={movie.name}   
           />
         ))}
 
       </div>
+        {/* you can put any youtube id to play song on the page */}
+        {/* <Youtube videoId="tUUElxEGo0U" opts={opts} />  */}
 
-      
+        {trailerUrl && <Youtube videoId={trailerUrl}f opts={opts} /> 
+      }
     </div>
   )
   
